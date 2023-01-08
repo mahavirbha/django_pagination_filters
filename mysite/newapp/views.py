9efe492c07bee566269b29c2719beebe3d3a110b
Mahavirsinh Chauhan <100768509+mahavir-amoga@users.noreply.github.com>
2023-01-09 00:34:18 +0530

    pagination added through url

--- mysite/newapp/views.py
+++ mysite/newapp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Movies
+from django.core.paginator import Paginator
 
 # Create your views here.
 def movie_list(req):
     movie_objects = Movies.objects.all()
+
+    paginator = Paginator(movie_objects,3)
+    page = req.GET.get('page')
+    movie_objects = paginator.get_page(page)
+
     return render(req, 'newapp/movie_list.html', {'movie_objects': movie_objects})
