936ee764cbf928540e56805b26481ee1c0296aa7
Mahavirsinh Chauhan <100768509+mahavir-amoga@users.noreply.github.com>
2023-01-16 22:25:32 +0530

    search & filter added

--- mysite/newapp/views.py
+++ mysite/newapp/views.py
@@ -6,6 +6,11 @@ from django.core.paginator import Paginator
 def movie_list(req):
     movie_objects = Movies.objects.all()
 
+    # search & filter
+    movie_name = req.GET.get('movie_name')
+    if movie_name != '' and movie_name is not None:
+        movie_objects = movie_objects.filter(name__icontains=movie_name)
+
     paginator = Paginator(movie_objects,3)
     page = req.GET.get('page')
     movie_objects = paginator.get_page(page)
